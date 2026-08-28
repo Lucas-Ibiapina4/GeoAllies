c46851241a826314ad0140d61f66044b7e179366
Lucas-Ibiapina4 <lucas.ibiapina04@gmail.com>
2026-08-28 11:32:49 -0300

    Initial Commit

---
+++ GeoAllies/GeoAlliesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoAlliesApp.swift
+//  GeoAllies
+//
+//  Created by Lucas Ibiapina on 28/08/26.
+//
+
+import SwiftUI
+
+@main
+struct GeoAlliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
