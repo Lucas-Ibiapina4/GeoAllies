e7ed443a869af0f67ac63f57809adab26ef026cd
AgnesRistau <agnesristau34@gmail.com>
2026-08-31 11:29:14 -0300

    Tela inicial do app

---
+++ GeoAllies/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  GeoAllies
+//
+//  Created by Agnes Pontes Ristau on 31/08/26.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
