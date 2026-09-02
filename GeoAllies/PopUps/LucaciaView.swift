47ff6026aae0a57833b13e3f1ea69d8cae959cf6
AgnesRistau <agnesristau34@gmail.com>
2026-09-02 11:07:27 -0300

    Fiz o pop up do País do usuario

---
+++ GeoAllies/PopUps/LucaciaView.swift
@@ -0,0 +1,18 @@
+//
+//  LucaciaView.swift
+//  GeoAllies
+//
+//  Created by Agnes Pontes Ristau on 02/09/26.
+//
+
+import SwiftUI
+
+struct LucaciaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LucaciaView()
+}
