f27e0138ec64d0c29c48fa0774749038484c5d1b
Bianca Moura <sbianca1303@gmail.com>
2026-09-05 14:08:26 -0300

    Atualização

--- GeoAllies/Datas/GameManager.swift
+++ GeoAllies/Datas/GameManager.swift
@@ -8,9 +8,8 @@
 import Observation
 import SwiftUI
 
-////avisa quando os dados mudam
+//avisa quando os dados mudam
 @Observable
-
 //game manager que permite controlar melhor os fluxos do jogo alternando entre os estados de aliança e dados específicos dos países
 class GameManager {
     var yourCountry: Country
@@ -22,9 +21,9 @@ class GameManager {
     //setamento dos valores do pilares de cada país
     init() {
         self.yourCountry = Country(economia: 0, militarismo: 0, tecnologia: 0)
-        self.agnolia = Country(economia: 8, militarismo: 2, tecnologia: 3)
-        self.cuastria = Country(economia: 3, militarismo: 9, tecnologia: 1)
-        self.lucacia = Country(economia: 1, militarismo: 4, tecnologia: 10)
+        self.agnolia = Country(economia: 5, militarismo: 1, tecnologia: 1)
+        self.cuastria = Country(economia: 1, militarismo: 5, tecnologia: 1)
+        self.lucacia = Country(economia: 1, militarismo: 1, tecnologia: 5)
     }
     
     func aliar(_ country: Country) {
--- GeoAllies/Models/Country.swift
+++ GeoAllies/Models/Country.swift
@@ -1,8 +1,8 @@
 //
-//  Countrys.swift
+//  Country.swift
 //  GeoAllies
 //
-//  Created by Lucas Ibiapina on 31/08/26.
+//  Created by Bianca Moura on 05/09/26.
 //
 
 import SwiftUI
@@ -10,16 +10,17 @@ import SwiftData
 
 @Model
 class Country {
-    var economia: Int = 0
-    var militarismo: Int = 0
-    var tecnologia: Int = 0
     
     var id: UUID = UUID()
-
+    
+    var economia : Int = 0
+    var militarismo : Int = 0
+    var tecnologia : Int = 0
+    
     init(economia: Int, militarismo: Int, tecnologia: Int){
+        
         self.economia = economia
         self.militarismo = militarismo
         self.tecnologia = tecnologia
     }
-    
 }
